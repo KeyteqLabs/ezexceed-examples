6fc70dbd6c5f1d0a46cbb776ad613fdefd93d47e
Raymond Julin <raymond.julin@gmail.com>
2013-05-30 12:42:22 +0200

    Added a cloud animation example to show you can do whatever you want in apps

---
+++ design/ezexceed/javascript/clouds.js
@@ -0,0 +1,80 @@
+define(['underscore', 'backbone', 'jquery-safe'], function(_, Backbone, $)
+{
+    return Backbone.View.extend({
+        initialize: function()
+        {
+            _.bindAll(this);
+            this.images = {};
+        },
+
+        events : {
+            'click button' : function(e) {
+                this.running = !this.running;
+                if (this.running) this.animate();
+            }
+        },
+
+        render: function()
+        {
+            this.trigger('loaded');
+            this.$el
+                .html('<canvas id="clouds"></canvas>')
+                .prepend("<button>Toggle</button>")
+                .append("FUCK YEAH");
+
+            // Canvas setup
+            this.h = this.$el.height();
+            this.w = this.$el.width();
+            this.canvas = this.$('canvas')[0];
+            this.canvas.width = this.w;
+            this.canvas.height = this.h;
+            this.context = this.canvas.getContext('2d');
+
+            // Bootstrap cloud image
+            this._image = new Image;
+            this._image.src = '/extension/ezexceed/design/ezexceed/images/kp/128x128/Cloud.png';
+
+            return this;
+        },
+
+
+        animate : function(time)
+        {
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            _.range(10).map(this.addImage);
+            if (this.running) {
+                requestAnimationFrame(this.animate);
+            }
+        },
+
+        addImage : function(num) {
+            var image = this.image(num);
+            if (image.x && image.y) {
+                // If out of bounds on right
+                if (image.x + image.movement > (this.w - image.size)) {
+                    image.direction = -1;
+                }
+                else if (image.x - image.movement < 0) {
+                    image.direction = 1;
+                }
+                image.x = image.x + (image.direction * image.movement);
+            }
+            else {
+                image.x = parseInt(Math.random(0, 1) * this.w * .9, 10);
+                image.y = parseInt(Math.random(0, 1) * this.h * .9, 10);
+            }
+            this.context.drawImage(this._image, image.x, image.y, image.size, image.size);
+        },
+
+        image : function(id) {
+            if (!_.has(this.images, id)) {
+                this.images[id] = {
+                    size: [32, 48, 128][parseInt(Math.random(0,1) * 3, 10)],
+                    movement : 1 + parseInt(Math.random(0, 1) * 10),
+                    direction : id % 2 ? 1 : -1
+                };
+            }
+            return this.images[id];
+        }
+    });
+});
--- settings/ezexceed.ini.append.php
+++ settings/ezexceed.ini.append.php
@@ -2,9 +2,17 @@
 
 [Toolbar]
 Buttons[]=twitter
+Buttons[]=clouds
 
 [Toolbar_twitter]
-Name=Tweets
-Icon=Share
+Name=Twitter
+Icon=Umbrella
 URL=/extension/ezexceed-examples/design/ezexceed/lib/twitter/widget.html
 Type=popover
+
+[Toolbar_clouds]
+Name=Clouds
+Icon=Cloud
+Module=ezexceed-examples/clouds
+NS=ezexceed-examples
+Type=stack
